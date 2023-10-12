6ebe80ad60342a8a321341aac266347b072063f9
Sahil Udayasingh <sahil.u@chalo.com>
2023-10-12 19:57:44 +0530

    Added logs to the system

--- main.go
+++ main.go
@@ -2,15 +2,34 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, World!")
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Log the timestamp and request details
+	log.Printf("[%s] %s %s", time.Now().Format("2006-01-02 15:04:05"), r.Method, r.URL.Path)
+
+	// Your request handling logic here
+	fmt.Fprintf(w, "Hello, World!")
 }
 
 func main() {
-	http.HandleFunc("/", helloHandler)
-	fmt.Println("Server is running on :8080...")
+	// Create a custom logger that writes log entries to a file
+	logFile, err := os.Create("server.log")
+	if err != nil {
+		log.Fatal("Error creating log file:", err)
+	}
+	defer logFile.Close()
+	logger := log.New(logFile, "", log.LstdFlags)
+
+	// Set the custom logger as the default logger
+	log.SetOutput(logger.Writer())
+
+	http.HandleFunc("/", handler)
+
+	fmt.Println("Server is running on :8080")
 	http.ListenAndServe(":8080", nil)
 }
